72768088016a5c09b3e5b514e49738cfc8c049d5
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-03 17:10:04 +0900

    Added: handle_error()

---
+++ src/error.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 16:53:25 by squinn            #+#    #+#             */
+/*   Updated: 2025/08/03 17:04:25by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/fdf.h"
+
+void handle_error(const char *message) {
+  if (errno > 0)
+    perror(message);
+  else
+    ft_putendl_fd(message, STDERR_FILENO);
+}
