8fb8b259e50a013e5d2d6c4bbe537c0897686293
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-04 17:19:33 +0900

    Added: main.c

---
+++ src/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 16:23:43 by squinn            #+#    #+#             */
+/*   Updated: 2025/08/04 13:01:04 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/fdf.h"
+
+int main(char argc, char *argv[])
+{
+  // TODO: error handling
+  if (argc != 2)
+    handle_error("");
+  // Read file and allocate data structure
+  // Transform isometric view
+  // Initialize minilibx variables
+  // Draw line
+  // Free resources
+  exit(EXIT_SUCCESS);
+}
