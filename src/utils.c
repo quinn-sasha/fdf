beeda7ee798e6d5494277983be37e058c16279c0
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-05 18:38:51 +0900

    Added: make_upper()

--- src/utils.c
+++ src/utils.c
@@ -6,10 +6,15 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 17:06:13 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/04 17:13:45 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/05 18:35:57 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
 t_point new_point(int x, int y, int z, uint32_t color);
+
+void make_upper(unsigned int i, char *c) {
+  (void)i;
+  ft_toupper(*c);
+}
