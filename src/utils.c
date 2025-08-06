de3c87c6610f55d49a681b6fc09a621a183d8daf
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-06 09:53:32 +0900

    Added: new_point()

--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,22 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 17:06:13 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/05 18:35:57 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/06 09:52:01 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
-t_point new_point(int x, int y, int z, uint32_t color);
+t_point new_point(int x, int y, int z, uint32_t color) {
+  t_point point;
+  point.x = x;
+  point.y = y;
+  point.z = z;
+  point.transformed_x = NULL;
+  point.transformed_y = NULL;
+  point.color = color;
+  return point;
+}
 
 void make_upper(unsigned int i, char *c) {
   (void)i;
