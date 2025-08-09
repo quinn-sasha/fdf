aedb024825c26219b01d2207867b8ee7bf822698
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-09 20:14:27 +0900

    Moved get_color_gradient() to color.c

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 17:06:13 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/09 16:58:21 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/09 20:14:06 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,6 @@ t_point new_point(double x, double y, double z, uint32_t color) {
   point.transformed_z = -1;
   point.color = color;
   return point;
-}
-
-uint32_t get_color_gradient(t_point a, t_point b, int position, int is_gentle) {
-  if (a.color == b.color)
-    return a.color;
-  int ratio;
-  if (is_gentle)
-    ratio = (position - a.x) / (b.x - a.x);
-  else
-    ratio = (position - a.y) / (b.y - a.y);
-  uint32_t color = a.color + ratio * (b.color - a.color);
-  return color;
 }
 
 // dx cannot be 0
