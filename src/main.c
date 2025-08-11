8af28b1e84b0721e3b2c894f12b3fba9238edbfb
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-11 12:19:26 +0900

    Fixed: typo in main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/03 16:23:43 by squinn            #+#    #+#             /*   Updated: 2025/08/08 17:14:58 by squinn           ###   ########.fr       */
+/*   Created: 2025/08/11 12:18:10 by squinn            #+#    #+#             */
+/*   Updated: 2025/08/11 12:18:59 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +34,9 @@ void init_mlx_resources(t_data *data) {
   (void)image->endian;
 }
 
-int main(char argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  if (argc != 2 || !is_valid_filename(argv[1]));
+  if (argc != 2 || !is_valid_filename(argv[1]))
     handle_error(FORMAT);
   t_map map;
   init_map(&map, argv[1]);
