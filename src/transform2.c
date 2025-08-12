4f1b6793dafe971581cfb823eb50acf764d79fbd
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-12 11:47:45 +0900

    Fixed: norm

--- src/transform2.c
+++ src/transform2.c
@@ -12,62 +12,94 @@
 
 #include "../include/fdf.h"
 
-void rotate_around_z_axis(t_map *map, double angle) {
-  int row = 0;
-  while (row < map->num_rows) {
-    int col = 0;
-    while (col < map->num_cols) {
-      t_point *point = &(map->grid[row][col]);
-      double x = map->grid[row][col].x;
-      double y = map->grid[row][col].y;
-      point->x = x * cos(angle) - y * sin(angle);
-      point->y = x * sin(angle) + y * cos(angle);
-      col++;
-    }
-    row++;
-  }
+void	rotate_around_z_axis(t_map *map, double angle)
+{
+	int		row;
+	int		col;
+	t_point	*point;
+	double	x;
+	double	y;
+
+	row = 0;
+	while (row < map->num_rows)
+	{
+		col = 0;
+		while (col < map->num_cols)
+		{
+			point = &(map->grid[row][col]);
+			x = map->grid[row][col].x;
+			y = map->grid[row][col].y;
+			point->x = x * cos(angle) - y * sin(angle);
+			point->y = x * sin(angle) + y * cos(angle);
+			col++;
+		}
+		row++;
+	}
 }
 
-void rotate_around_x_axis(t_map *map, double angle) {
-  int row = 0;
-  while (row < map->num_rows) {
-    int col = 0;
-    while (col < map->num_cols) {
-      t_point *point = &(map->grid[row][col]);
-      double y = map->grid[row][col].y;
-      double z = map->grid[row][col].z;
-      point->y = y * cos(angle) - z * sin(angle);
-      point->z = y * sin(angle) + z * cos(angle);
-      col++;
-    }
-    row++;
-  }
+void	rotate_around_x_axis(t_map *map, double angle)
+{
+	int		row;
+	int		col;
+	t_point	*point;
+	double	y;
+	double	z;
+
+	row = 0;
+	while (row < map->num_rows)
+	{
+		col = 0;
+		while (col < map->num_cols)
+		{
+			point = &(map->grid[row][col]);
+			y = map->grid[row][col].y;
+			z = map->grid[row][col].z;
+			point->y = y * cos(angle) - z * sin(angle);
+			point->z = y * sin(angle) + z * cos(angle);
+			col++;
+		}
+		row++;
+	}
 }
 
-void translate(t_map* map, double dx, double dy) {
-  int row = 0;
-  while (row < map->num_rows) {
-    int col = 0;
-    while (col < map->num_cols) {
-      t_point *point = &(map->grid[row][col]);
-      point->x += dx;
-      point->y += dy;
-      col++;
-    }
-    row++;
-  }
+void	translate(t_map *map, double dx, double dy)
+{
+	int		row;
+	int		col;
+	t_point	*point;
+
+	row = 0;
+	while (row < map->num_rows)
+	{
+		col = 0;
+		while (col < map->num_cols)
+		{
+			point = &(map->grid[row][col]);
+			point->x += dx;
+			point->y += dy;
+			col++;
+		}
+		row++;
+	}
 }
 
-void scale(t_map *map, double rate) {
-  int row = 0;
-  while (row < map->num_rows) {
-    int col = 0;
-    while (col < map->num_cols) {
-      t_point *point = &(map->grid[row][col]);
-      point->x *= rate;
-      point->y *= rate;
-      col++;
-    }
-    row++;
-  }
+void	scale(t_map *map, double rate)
+{
+	int		row;
+	int		col;
+	t_point	*point;
+
+	row = 0;
+	while (row < map->num_rows)
+	{
+		col = 0;
+		while (col < map->num_cols)
+		{
+			point = &(map->grid[row][col]);
+			point->x *= rate;
+			point->y *= rate;
+			col++;
+		}
+		row++;
+	}
 }
