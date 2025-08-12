2abc166d28fc01100f4951e5c1229e1419fb79e6
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-12 12:10:28 +0900

    Deleted: test codes

--- tests/test_parse_color.c
+++
@@ -1,8 +0,0 @@
-#include "../include/fdf.h"
-
-int main(int argc, char *argv[]) {
-  (void)argc;
-  char *color_info = argv[1];
-  uint32_t color = parse_color(color_info);
-  printf("color: %8X\n", color);
-}
